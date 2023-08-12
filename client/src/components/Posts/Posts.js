39407306dad1e51e6bb2694bbd33669f06b603c0
Zeyad Hegazy <zhegazy409@gmail.com>
2023-08-12 11:08:30 +0300

    add and fetch posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,22 +1,30 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
+import { Grid, CircularProgress } from "@material-ui/core";
+
 import Post from "./Post/Post";
 import useStyles from "./styles";
 
 const Posts = () => {
 	const classes = useStyles();
-
 	const posts = useSelector((state) => state.posts);
-	console.log(posts);
 
-	return (
-		<>
-			<h1>Posts</h1>
-			<Post />
-			<Post />
-			<Post />
-		</>
+	return !posts.length ? (
+		<CircularProgress />
+	) : (
+		<Grid
+			container
+			className={classes.mainContainer}
+			alignItems="stretch"
+			spacing={3}
+		>
+			{posts.map((post) => (
+				<Grid key={post._id} item xs={12} sm={6}>
+					<Post post={post} />
+				</Grid>
+			))}
+		</Grid>
 	);
 };
 
