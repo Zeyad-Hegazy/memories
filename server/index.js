9dee9531fa7382ac15e14d2fff1d1b443efb1eb2
Zeyad Hegazy <zhegazy409@gmail.com>
2023-08-14 15:08:33 +0300

    make it responsive

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,12 +14,10 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-	"mongodb+srv://zeyadMemo:vkvj8AMKhVBJWbJV@cluster0.as9h9zy.mongodb.net/?retryWrites=true&w=majority";
-
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
-mongoose	
+mongoose
 	.connect(CONNECTION_URL, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
